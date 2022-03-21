acd7b8140ffe435c0589e8188cb675ee8c740d1a
usaurioRAWR <usauriorawr@gmail.com>
2022-03-21 07:21:51 +0100

    neoRAWRvim

---
+++ README 7.19.27.md
@@ -0,0 +1,9 @@
+# neoRAWRvim
+
+Proyecto de para el Segundo Trimestre.
+
+Esta pagina a sido desarrollada con código abierto y mis conocimientos adquiridos en el modulo Lenguajes de marcas y sistemas de gestión de información. 
+
+## Validación de CSS del W3C
+Resultados del Validador CSS del W3C para https://neorawrvim.000webhostapp.com/index.html (CSS versión 3 + SVG)
+![Validacion_de_CSS_del_W3C](/Validacion_de_CSS_del_W3C.png)
---
+++ js/copy.js
@@ -0,0 +1,149 @@
+function myFunction() {
+  /* Get the text field */
+  var copyText = document.getElementById("myInput");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+  
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
+
+function copy_nvim_tu_archivo_txt() {
+  /* Get the text field */
+  var copyText = document.getElementById("nvim_tu_archivo_txt");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+  
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
+
+function copy_smile() {
+  /* Get the text field */
+  var copyText = document.getElementById("smile");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+  
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
+
+function copy_q() {
+  /* Get the text field */
+  var copyText = document.getElementById("q");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+  
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
+
+function copy_q_exclamacion() {
+  /* Get the text field */
+  var copyText = document.getElementById("q_exclamacion");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+  
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
+
+function copy_w() {
+  /* Get the text field */
+  var copyText = document.getElementById("w");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+  
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
+
+function copy_wq() {
+  /* Get the text field */
+  var copyText = document.getElementById("wq");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+  
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
+
+function copy_w_nombre_fichero() {
+  /* Get the text field */
+  var copyText = document.getElementById("w_nombre_fichero");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+  
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
+
+function copy_wq_nombre_fichero() {
+  /* Get the text field */
+  var copyText = document.getElementById("wq_nombre_fichero");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+  
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
+
+function copy_wqa() {
+  /* Get the text field */
+  var copyText = document.getElementById("wqa");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+  
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
